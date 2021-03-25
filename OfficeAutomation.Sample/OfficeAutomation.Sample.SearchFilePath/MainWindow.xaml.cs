3af81760ceff56417f5e41dc0caa0ac971db1337
kimhyesu <juid00@naver.com>
2021-03-25 12:47:50 +0900

    Create Project

---
+++ OfficeAutomation.Sample/OfficeAutomation.Sample.SearchFilePath/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace OfficeAutomation.Sample.SearchFilePath
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDlg = new OpenFileDialog();
+			openFileDlg.InitialDirectory = GetIoFilePath(); //csv의 이름이 필요하다.
+			openFileDlg.Filter = "*.csv|";
+			lbPath.Content = openFileDlg.InitialDirectory.ToString(); 
+		}
+
+		private string GetIoFilePath()
+		{
+			string path = string.Empty;
+
+			string workingDirectory = Environment.CurrentDirectory;
+			path = Directory.GetParent(workingDirectory).Parent.Parent.FullName; //  + "\\IO_File"
+
+			return path;
+		}
+	}
+}
